f7a6b7d943941fce446051223f343d62750de32a
CCamsyn <2742046922@qq.com>
2024-08-24 21:01:06 +0800

    [init]: frist commit to initialize dev env for XSan     We initialize XSan from parts of LLVM. In details, we pick up codes about     - ASan: runtime & instrumentation     - UBSan: runtime     - TSan: runtime & instrumentation     - MSan: runtime & instrumentation     - else common dependencies These codes are mainly from `compiler-rt/lib` and `llvm/lib/Transforms/Instrumentation`.
    We extract the above codes and re-organize them
    in different project structure, which consists of
        - runtime: contains sanitizers runtimes
        - insturmentation: contains sanitizer passes
        - compiler: a clang wrapper

---
+++ src/instrumentation/AttributeTaggingPass.cpp
@@ -0,0 +1,37 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is to define a transformation pass that adds attribute tags to
+// functions and instructions. Because the sanitizer passes only instrument 
+// those functions with appropriate attribute.
+// - AddressSanitizer: attribute `sanitize_address`
+// - ThreadSanitizer: attribute `sanitize_address`
+//
+//===----------------------------------------------------------------------===//
+
+#include "AttributeTaggingPass.hpp"
+#include "llvm/IR/Attributes.h"
+
+using namespace llvm;
+
+
+AttributeTaggingPass::AttributeTaggingPass(SanitizerType sanTy): _sanTy(sanTy) {}
+
+PreservedAnalyses AttributeTaggingPass::run(Module &M, ModuleAnalysisManager &_) {
+    for (auto &F : M) {
+        switch (_sanTy) {
+        case ASan:
+            F.addFnAttr(Attribute::SanitizeAddress);   
+          break;
+        case TSan:
+            F.addFnAttr(Attribute::SanitizeThread);
+          break;
+        case XSan:
+            // FIXME: do not hard code embedding!
+            F.addFnAttr(Attribute::SanitizeAddress);   
+            F.addFnAttr(Attribute::SanitizeThread);
+          break;
+        }
+    }
+    
+    return PreservedAnalyses::all();
+}
---
+++ src/instrumentation/AttributeTaggingPass.hpp
@@ -0,0 +1,39 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is to define a transformation pass that adds attribute tags to
+// functions and instructions. Because the sanitizer passes only instrument 
+// those functions with appropriate attribute.
+// - AddressSanitizer: attribute `sanitize_address`
+// - ThreadSanitizer: attribute `sanitize_address`
+//
+//===----------------------------------------------------------------------===//
+#pragma once
+
+#include "llvm/IR/PassManager.h"
+#include "llvm/Transforms/Instrumentation/AddressSanitizerOptions.h"
+
+namespace llvm {
+class Module;
+class raw_ostream;
+
+
+enum SanitizerType {
+    ASan, TSan, XSan
+};
+
+
+
+/// This pass tag each function with proper sanitizer attribute.
+class AttributeTaggingPass
+    : public PassInfoMixin<AttributeTaggingPass> {
+public:
+  AttributeTaggingPass(SanitizerType);
+  PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM);
+
+  static bool isRequired() { return true; }
+private:
+  SanitizerType _sanTy;
+};
+
+
+} // namespace llvm
