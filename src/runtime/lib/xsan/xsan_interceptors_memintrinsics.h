d5b57ae3dbbbbbea5814be32793a0199113add01
CCamsyn <2742046922@qq.com>
2024-09-29 23:26:18 +0800

    fix(runtime): deduplicate the macro definitions

--- src/runtime/lib/xsan/xsan_interceptors_memintrinsics.h
+++ src/runtime/lib/xsan/xsan_interceptors_memintrinsics.h
@@ -2,6 +2,7 @@
 
 #include "asan/orig/asan_internal.h"
 #include "asan/asan_mapping.h"
+#include "asan/asan_interceptors_memintrinsics.h"
 
 #include "xsan_interface_internal.h"
 #include "xsan_internal.h"
@@ -12,35 +13,6 @@
 
 DECLARE_REAL(void*, memcpy, void *to, const void *from, uptr size)
 DECLARE_REAL(void*, memset, void *block, int c, uptr size)
-
-namespace __asan {
-
-// Return true if we can quickly decide that the region is unpoisoned.
-// We assume that a redzone is at least 16 bytes.
-static inline bool AsanQuickCheckForUnpoisonedRegion(uptr beg, uptr size) {
-  if (size == 0) return true;
-  if (size <= 32)
-    return !AddressIsPoisoned(beg) &&
-           !AddressIsPoisoned(beg + size - 1) &&
-           !AddressIsPoisoned(beg + size / 2);
-  if (size <= 64)
-    return !AddressIsPoisoned(beg) &&
-           !AddressIsPoisoned(beg + size / 4) &&
-           !AddressIsPoisoned(beg + size - 1) &&
-           !AddressIsPoisoned(beg + 3 * size / 4) &&
-           !AddressIsPoisoned(beg + size / 2);
-  return false;
-}
-
-// Behavior of functions like "memcpy" or "strcpy" is undefined
-// if memory intervals overlap. We report error in this case.
-// Macro is used to avoid creation of new frames.
-static inline bool AsanRangesOverlap(const char *offset1, uptr length1,
-                                 const char *offset2, uptr length2) {
-  return !((offset1 + length1 <= offset2) || (offset2 + length2 <= offset1));
-}
-
-}
 
 namespace __xsan {
 
