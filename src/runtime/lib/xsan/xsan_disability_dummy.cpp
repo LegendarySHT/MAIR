7734aa4a60c37e746b1294232346c067097ff004
CCamsyn <2742046922@qq.com>
2024-10-05 20:29:24 +0800

    fix(runtime): provide empty definitions for some symbols temporarily
    As tsan_interceptor_posix.cpp is disabled, some symbols lack definitions.
    Therefore, we provide the empty implementations temporarily.
    
    We intend to implement the relevant symbols in the future.

--- src/runtime/lib/xsan/xsan_disability_dummy.cpp
+++ src/runtime/lib/xsan/xsan_disability_dummy.cpp
@@ -1,6 +1,37 @@
+/**
+ This file provide the empty definitions temporarily.
+
+ Some original sanitizer source code references these functions,
+    but they are not used by XSan.
+*/
+
+#include "xsan_internal.h"
+
+/// TODO: Delegate these function implemantations to XSan or remove 
+/// all the references to these symbols.
 namespace __asan {
 
 void InitializeAsanInterceptors() {}
 void ReplaceSystemMalloc() {}
 
 }  // namespace __asan
+
+namespace __sanitizer {
+    class LibIgnore; 
+}
+
+namespace __tsan{
+
+
+class ThreadState;
+
+/* In tsan_interceptors_posix.cpp */
+void PlatformCleanUpThreadState(ThreadState *thr) {}
+__sanitizer::LibIgnore *libignore() { return nullptr; }
+void ProcessPendingSignalsImpl(ThreadState *thr) {}
+void InitializeInterceptors() {}
+void InitializeLibIgnore() {}
+}
+
+/* In tsan_interceptors_posix.cpp */
+extern "C" void __tsan_setjmp(uptr) { }
