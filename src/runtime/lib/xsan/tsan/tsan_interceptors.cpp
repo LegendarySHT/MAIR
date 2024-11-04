425b2c665cee1ca333151f4db7c3f00bb70395b0
CCamsyn <2742046922@qq.com>
2024-11-05 00:00:38 +0800

    fix(runtime): relax interceptor of `munmap`
    TSan's `munmap` interceptor crashes asan_and_llvm_coverage_test.cpp.
    
    I have no idea what happens, thus temporarily use the real `munmap`
    to replace TSan's interceptor

--- src/runtime/lib/xsan/tsan/tsan_interceptors.cpp
+++ src/runtime/lib/xsan/tsan/tsan_interceptors.cpp
@@ -697,8 +697,9 @@ static void *mmap_interceptor(ThreadState *thr, uptr pc, Mmap real_mmap,
 }
 
 TSAN_INTERCEPTOR(int, munmap, void *addr, long_t sz) {
-  SCOPED_TSAN_INTERCEPTOR(munmap, addr, sz);
-  UnmapShadow(thr, (uptr)addr, sz);
+  /// FIXME: this interceptor crashes asan_and_llvm_coverage_test.cpp
+  // SCOPED_TSAN_INTERCEPTOR(munmap, addr, sz);
+  // UnmapShadow(thr, (uptr)addr, sz);
   int res = REAL(munmap)(addr, sz);
   return res;
 }
@@ -2620,7 +2621,7 @@ void InitializeInterceptors() {
 //   TSAN_INTERCEPT(reallocarray);
 //   TSAN_INTERCEPT(free);
 //   TSAN_INTERCEPT(cfree);
-//   TSAN_INTERCEPT(munmap);
+  TSAN_INTERCEPT(munmap);
 //   TSAN_MAYBE_INTERCEPT_MEMALIGN;
 //   TSAN_INTERCEPT(valloc);
 //   TSAN_MAYBE_INTERCEPT_PVALLOC;
