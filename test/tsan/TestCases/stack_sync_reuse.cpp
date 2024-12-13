814789a066f85a17febd48435ee9da0aab1309d4
CCamsyn <2742046922@qq.com>
2024-12-13 22:13:40 +0800

    fix(test): fixes FP caused by fake stack
    Fixes stack_sync_reuse.cpp (TSan's testcase).
    
    ASan's fake stack frame caused the exceptional non-reuse of stack frame,
     further causing the abnormal program termination without recycle the
    thread. And thus, a FP thread leak occurs.
    
    The solution is quite straightforward, i.e., disable Fake Stack by
    transfering environment variable of
    `ASAN_OPTIONS=detect_stack_use_after_return=0` when executing the test.

--- test/tsan/TestCases/stack_sync_reuse.cpp
+++ test/tsan/TestCases/stack_sync_reuse.cpp
@@ -1,4 +1,4 @@
-// RUN: %clang_tsan -O1 %s -o %t && %run %t 2>&1 | FileCheck %s
+// RUN: %clang_tsan -O1 %s -o %t && ASAN_OPTIONS=detect_stack_use_after_return=0 %run %t 2>&1 | FileCheck %s
 #include "test.h"
 
 // Test case https://github.com/google/sanitizers/issues/494
