8aa1d14dcbfc605cc7ab2b74620935f97142bcf0
CCamsyn <2742046922@qq.com>
2024-12-31 19:25:57 +0800

    upd(runtime): update xsan_preinit.cpp

--- src/runtime/lib/xsan/xsan_preinit.cpp
+++ src/runtime/lib/xsan/xsan_preinit.cpp
@@ -13,11 +13,9 @@ extern "C" {
 extern void __xsan_init();
 }
 
-// The symbol is called __local_xsan_preinit, because it's not intended to be
-// exported.
-// This code linked into the main executable when -fsanitize=thread is in
-// the link flags. It can only use exported interface functions.
-__attribute__((section(".preinit_array"),
-               used)) void (*__local_xsan_preinit)(void) = __xsan_init;
+// This section is linked into the main executable when -fsanitize=address is
+// specified to perform initialization at a very early stage.
+__attribute__((section(".preinit_array"), used)) static auto preinit =
+    __xsan_init;
 
 #endif
